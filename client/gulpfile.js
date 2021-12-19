8861006b92b218aa7b634d6960ec43228e2b06bb
shahedsa <shahedsa@stud.ntnu.no>
2021-12-19 23:31:07 +0100

    Header comp commit

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -3,6 +3,6 @@ const gulpSass = require('gulp-sass')
 const sass = gulpSass(require('sass'))
 // vi lager en script i json file for å kjøre sass funksjon
 gulp.task('sass', async function (){
-    gulp.src('src/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+    gulp.src('src/Components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
 
 })
